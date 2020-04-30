8621fee711be05ae026c6356fcd7ee5d0314f8fb
Danial Qureshi <danialqureshi.m@gmail.com>
2020-04-30 17:04:51 -0400

    added client folder as frontend and updated serverside package.json files

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ require('dotenv').config();
 const app = express();
 
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'));
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend','build','index.html'))
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
     })
 }
 
